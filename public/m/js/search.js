73ab7d7f1d28fd5babfdc2806e16d241440645cf
拿铁自然卷 <2895358020@qq.com>
2018-11-20 01:01:40 +0800

    第一次优化

---
+++ public/m/js/search.js
@@ -0,0 +1,71 @@
+$(function () {
+    var letao = function (getItem) {
+        this.getItem = getItem;
+    };
+    letao.prototype = {
+        /* 更新逻辑 */
+        refresh: function () {
+            if (localStorage.getItem('key')) {
+                var json = localStorage.getItem('key');
+                arr = JSON.parse(json);
+            } else {
+                arr = [];
+            };
+            var html = template('searchTpl', {
+                list: arr
+            });
+            $('.search-bottom').html(html);
+        },
+        /* 搜索逻辑 */
+        search: function () {
+            $('.btn-search').on('tap', function () {
+                var text = $.trim($('.search-text').val());
+                if (text == '') {
+                    alert('内容不能为空');
+                    return;
+                }
+                if (localStorage.getItem('key')) {
+                    var json = localStorage.getItem('key');
+                    arr = JSON.parse(json);
+                } else {
+                    arr = [];
+                }
+                if (arr.indexOf($('.search-text').val()) != -1) {
+                    arr.splice(arr.indexOf($('.search-text').val()), 1);
+                }
+                arr.unshift($('.search-text').val());
+                localStorage.setItem('key', JSON.stringify(arr));
+                $('.search-text').val('');
+                var html = template('searchTpl', {
+                    list: arr
+                });
+                $('.search-bottom').html(html);
+            })
+        },
+        /* 清空逻辑 */
+        clear: function () {
+            $('.search-remove').on('tap', ()=>{
+                if (localStorage.getItem('key')) {
+                    localStorage.removeItem('key');
+                    this.refresh();
+                }
+            })
+        },
+        /* 单行删除逻辑 */
+        del: function () {
+            $('.search-bottom').on('tap', '.btn-del',()=>{
+                var id = $(this).data('id');
+                var json = localStorage.getItem('key');
+                var arr = JSON.parse(json);
+                arr.splice(id, 1);
+                localStorage.setItem('key', JSON.stringify(arr));
+                this.refresh();
+            })
+        }
+    };
+    var Letao = new letao();
+    Letao.refresh();
+    Letao.search();
+    Letao.clear();
+    Letao.del();
+})
